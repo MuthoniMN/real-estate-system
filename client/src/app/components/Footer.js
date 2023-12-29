06d36ea0a96a637adffc26786b82a9d25d3cf8be
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-12-29 15:37:21 +0300

    styling footer and navbar

--- client/src/app/components/Footer.js
+++ client/src/app/components/Footer.js
@@ -3,8 +3,8 @@ export default function Footer() {
     const currentYear = today.getFullYear();
     return (
         <footer className="text-center py-4">
-            <p>Developed by <a href="https://github.com/MuthoniMN" className="text-theme-color hover:underline hover:font-bold">Michelle Ndiangui</a></p>
-            <p><a href="https://www.figma.com/community/file/1316765990281362046" className="text-theme-color hover:underline hover:font-bold">Figma Design</a></p>
+            <p>Developed by <a href="https://github.com/MuthoniMN" className="text-theme-color hover:underline hover:font-bold transition-all">Michelle Ndiangui</a></p>
+            <p><a href="https://www.figma.com/community/file/1316765990281362046" className="text-theme-color transition-all hover:underline hover:font-bold">Figma Design</a></p>
             <p>Copyright &copy; {currentYear}</p>
         </footer>
     )
--- client/src/app/components/Navbar.js
+++ client/src/app/components/Navbar.js
@@ -4,10 +4,12 @@ import Link from "next/link"
 export default function Navbar() {
     return (
         <nav className="flex py-4 px-8 justify-between items-center">
-            <div className="flex items-center gap-2">
-                <Image src="/Nyumbani-Real-Estate.png" alt="Nyumbani Real Estate" width={56} height={56} className="rounded" />
-                <span className="font-bold">Nyumbani<br />Real Estate</span>
-            </div>
+            <Link href={"/"}>
+                <div className="flex items-center gap-2">
+                    <Image src="/Nyumbani-Real-Estate.png" alt="Nyumbani Real Estate" width={56} height={56} className="rounded" />
+                    <span className="font-bold">Nyumbani<br />Real Estate</span>
+                </div>
+            </Link>
             <ul className="flex gap-4">
                 <li>
                     <Link href={"/"} className="font-bold">Home</Link>
