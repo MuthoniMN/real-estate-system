6e034a088c7389fc3b9817057fdb537a5b9a4e42
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-12-29 15:18:11 +0300

    hero section

--- client/src/app/components/Navbar.js
+++ client/src/app/components/Navbar.js
@@ -5,21 +5,21 @@ export default function Navbar() {
     return (
         <nav className="flex py-4 px-8 justify-between items-center">
             <div className="flex items-center gap-2">
-                <span className="font-bold">Nyumbani<br />Real Estate</span>
                 <Image src="/Nyumbani-Real-Estate.png" alt="Nyumbani Real Estate" width={56} height={56} className="rounded" />
+                <span className="font-bold">Nyumbani<br />Real Estate</span>
             </div>
             <ul className="flex gap-4">
                 <li>
-                    <Link href={"/"}>Home</Link>
+                    <Link href={"/"} className="font-bold">Home</Link>
                 </li>
                 <li>
-                    <Link href={"/properties/sale"}>For Sale</Link>
+                    <Link href={"/properties/sale"} className="hover:font-bold">For Sale</Link>
                 </li>
                 <li>
-                    <Link href={"/properties/rent"}>For Rent</Link>
+                    <Link href={"/properties/rent"} className="hover:font-bold">For Rent</Link>
                 </li>
                 <li>
-                    <Link href={"/signin"}>Sign In</Link>
+                    <Link href={"/signin"} className="hover:font-bold">Sign In</Link>
                 </li>
             </ul>
             <button className="bg-theme-color py-2 px-4">Add a Listing</button>
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -3,7 +3,10 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-
+      <section style={{ background: "url('https://images.unsplash.com/photo-1496151428281-1d6de673b58e?w=1500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NDl8fHxlbnwwfHx8fHw%3D')", height: "75vh", border: "inset 0 0 0 2000px rgba(0, 0, 0, 0.3)" }} className='bg-fixed bg-no-repeat bg-cover bg-top flex flex-col items-center justify-center'>
+        <h1 className='text-3xl text-theme-color font-bold'>Sell. Buy. Rent</h1>
+        <input placeholder='Enter a location' className='w-1/2 my-4 py-2 px-4' />
+      </section>
     </main>
   )
 }
