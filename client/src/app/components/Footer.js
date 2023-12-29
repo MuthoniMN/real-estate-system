a3bb25c01f51af33c6a4a35c94372c750a4cf5b5
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-12-29 15:34:26 +0300

    styling footer

--- client/src/app/components/Footer.js
+++ client/src/app/components/Footer.js
@@ -2,9 +2,9 @@ export default function Footer() {
     const today = new Date();
     const currentYear = today.getFullYear();
     return (
-        <footer>
-            <p>Developed by <a href="https://github.com/MuthoniMN">Michelle Ndiangui</a></p>
-            <p><a href="https://www.figma.com/community/file/1316765990281362046">Figma Design</a></p>
+        <footer className="text-center py-4">
+            <p>Developed by <a href="https://github.com/MuthoniMN" className="text-theme-color hover:underline hover:font-bold">Michelle Ndiangui</a></p>
+            <p><a href="https://www.figma.com/community/file/1316765990281362046" className="text-theme-color hover:underline hover:font-bold">Figma Design</a></p>
             <p>Copyright &copy; {currentYear}</p>
         </footer>
     )
