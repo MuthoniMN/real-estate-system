6dbfa3a292d89685fd7a90d80c24e04b4d1a69ff
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-07 06:15:57 +0300

    responsive hero section

--- src/app/properties/sale/page.js
+++ src/app/properties/sale/page.js
@@ -3,7 +3,7 @@ import SearchBar from "../../components/SearchBar";
 export default function Sale() {
     return (
         <main>
-            <section style={{ background: "url(https://images.unsplash.com/photo-1605146769289-440113cc3d00?w=1500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8b24lMjBzYWxlJTIwcHJvcGVydHl8ZW58MHx8MHx8fDA%3D)", height: "50vh" }} className='bg-fixed bg-no-repeat bg-cover bg-top flex flex-col items-center justify-center'>
+            <section style={{ background: "url(https://images.unsplash.com/photo-1605146769289-440113cc3d00?w=1500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8b24lMjBzYWxlJTIwcHJvcGVydHl8ZW58MHx8MHx8fDA%3D)" }} className='bg-fixed bg-no-repeat bg-cover bg-top flex flex-col items-center justify-center sm:h-[50vh] h-[80vh]'>
                 <h2 className="text-3xl font-bold mb-4">Houses on Sale</h2>
                 <p>Find a property that is on sale</p>
             </section>
