7ee37bf708e31220358d779fb840edd8d6bad41f
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-07 16:13:25 +0300

    databse connection

---
+++ src/app/utils/db.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+const uri = process.env.MONGODB_URI
+
+const connectToDB = async () => {
+    mongoose.set('strictQuery', true)
+
+    if (isConnected) {
+        console.log(`Connected at: ${mongoose.connection}`)
+        return;
+    }
+
+    try {
+        await mongoose.connect(uri, {
+            dbName: "nyumbani_system"
+        })
+        isConnected = true;
+        console.log(`Connected at: ${mongoose.connection}`)
+    } catch (err) {
+        console.error(err)
+    }
+}
