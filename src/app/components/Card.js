3707a0e60576a12039a1caa18d31c213476077f3
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-07 06:21:25 +0300

    responsive homepage

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export default function Card({ img, title, desc, link, btnText }) {
     return (
-        <div className="p-4 shadow-sm hover:shadow-lg md:w-1/5">
+        <div className="p-4 shadow-sm hover:shadow-lg md:w-[250px]">
             <Image src={img} alt={title} width={120} height={120} className="mx-auto" />
             <h3 className="text-xl font-bold text-theme-color text-center my-2">{title}</h3>
             <p>{desc}</p>
--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main>
       <section style={{ background: "url('https://images.unsplash.com/photo-1496151428281-1d6de673b58e?w=1500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NDl8fHxlbnwwfHx8fHw%3D')", height: "75vh", border: "inset 0 0 0 2000px rgba(0, 0, 0, 0.3)" }} className='bg-fixed bg-no-repeat bg-cover bg-top flex flex-col items-center justify-center'>
         <h1 className='text-3xl text-theme-color font-bold'>Sell. Buy. Rent</h1>
-        <form className="w-1/2 bg-white flex my-4 py-2 px-4 justify-between">
+        <form className="sm:w-1/2 w-[75vh] bg-white flex m-4 py-2 px-4 justify-between">
           <input placeholder='Enter a location' className='bg-transparent' />
           <button type='submit' style={{ width: "24px" }} className="text-theme-color">
             <FontAwesomeIcon icon={faSearch} />
@@ -15,7 +15,7 @@ export default function Home() {
       </section>
       <section className="p-4">
         <h2 className="text-theme-color font-bold text-3xl text-center">Services We Offer: </h2>
-        <section className="flex justify-center gap-6 py-4">
+        <section className="flex justify-center gap-6 py-4 flex-wrap">
           <Card img={"/buy.svg"} title={"Buy Properties"} desc={"Get access to quality vetted properties to buy at the best prices"} link="/properties/sale" btnText={"Buy a Property"} />
           <Card img={"/sell.svg"} title={"Sell Your Properties"} desc={"Sell your properties at the best prices to our clients"} link="/properties/add" btnText={"Sell a Property"} />
           <Card img={"/rent.svg"} title={"Rent a Property"} desc={"Get access to quality vetted properties to rent at the best prices"} link="/properties/rent" btnText={"Rent a Property"} />
