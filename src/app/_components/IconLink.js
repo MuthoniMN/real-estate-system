e327fba80e881e40815d3987b3439de3207f4696
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-09 22:33:42 +0300

    icon link component

---
+++ src/app/_components/IconLink.js
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Link from "next/link"
+
+export default function IconLink({ link, icon, linkDesc }) {
+    return (
+        <Link href={link} className="flex gap-2 items-center justify-between w-full">
+            <FontAwesomeIcon icon={icon} style={{ height: "32px" }} />
+            <span className="hidden md:block w-full">{linkDesc}</span>
+        </Link>
+    )
+}
