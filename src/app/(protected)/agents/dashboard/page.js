3975916ca77336f629a80a557653e239d67801dc
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-10 01:38:54 +0300

    agents sidebar and dashboard

---
+++ src/app/(protected)/agents/dashboard/page.js
@@ -0,0 +1,7 @@
+export default function AdminDashboard() {
+    return (
+        <div className="m-4 w-full">
+            <h2 className="text-3xl font-bold">Agent Dashboard</h2>
+        </div>
+    )
+}
