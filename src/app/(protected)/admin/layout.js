4d7bbc1007a771fc48d1e53dd74ba47c0212daee
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-09 23:42:49 +0300

    making the sidebar reusable

--- src/app/(protected)/admin/layout.js
+++ src/app/(protected)/admin/layout.js
@@ -1,14 +1,48 @@
 import Sidebar from "@/app/_components/Sidebar"
+import { faHouseChimneyUser, faHouseCircleCheck, faHouseCircleExclamation, faUser, faUserEdit, faUserPlus } from "@fortawesome/free-solid-svg-icons";
 
 export const metadata = {
     title: 'Nyumbani Admin Dashboard',
     description: 'You can manage your agents, verify your properties and view your analytics here',
 }
 
+const links = [
+    {
+        link: "/admin/property/unverified",
+        desc: "Unverified Properties",
+        icon: faHouseCircleExclamation
+    },
+    {
+        link: "/admin/property/verified",
+        desc: "Verified Properties",
+        icon: faHouseCircleCheck
+    },
+    {
+        link: "/admin/property/rentals",
+        desc: "Rental Properties",
+        icon: faHouseChimneyUser
+    },
+    {
+        link: "/admin/property/agents",
+        desc: "Agents",
+        icon: faUser
+    },
+    {
+        link: "/admin/property/accounts",
+        desc: "Add Admin Account",
+        icon: faUserPlus
+    },
+    {
+        link: "/change-password",
+        desc: "Change Password",
+        icon: faUserEdit
+    }
+]
+
 export default function AdminLayout({ children }) {
     return (
         <main className="flex gap-4" style={{ height: "100vh" }}>
-            <Sidebar />
+            <Sidebar content={links} />
             <section className="clear ms-[32%] lg:ms-[20%]">
                 {children}
             </section>
--- src/app/_components/Sidebar.js
+++ src/app/_components/Sidebar.js
@@ -2,16 +2,13 @@ import IconLink from "./IconLink";
 import Logo from "./Logo";
 import { faHouseChimneyUser, faHouseCircleCheck, faHouseCircleExclamation, faUser, faUserEdit, faUserPlus } from "@fortawesome/free-solid-svg-icons";
 
-export default function Sidebar() {
+export default function Sidebar({ content }) {
     return (
         <nav className="bg-theme-color/15 p-4 rounded-e-[50px] w-1/8 h-full p-8 flex flex-col gap-6 items-center fixed me-4">
             <Logo />
-            <IconLink link="/admin/property/unverified" linkDesc="Unverified Properties" icon={faHouseCircleExclamation} />
-            <IconLink link="/admin/property/verified" linkDesc="Verified Properties" icon={faHouseCircleCheck} />
-            <IconLink link="/admin/property/rentals" linkDesc="Rental Properties" icon={faHouseChimneyUser} />
-            <IconLink link="/admin/property/agents" linkDesc="Agents" icon={faUser} />
-            <IconLink link="/admin/property/accounts" linkDesc="Add Admin Account" icon={faUserPlus} />
-            <IconLink link="/change-password" linkDesc="Change Password" icon={faUserEdit} />
+            {content.map((link, index) => {
+                return <IconLink link={link.link} linkDesc={link.desc} icon={link.icon} key={index} />
+            })}
         </nav>
     )
 }
