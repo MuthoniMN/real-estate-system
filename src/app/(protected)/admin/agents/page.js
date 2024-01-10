36eeeba888baede2753892bf09e27f485d117648
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-10 17:39:09 +0300

    all agents

--- src/app/(protected)/admin/agents/page.js
+++ src/app/(protected)/admin/agents/page.js
@@ -1,7 +1,32 @@
-export default function Agents() {
+import CardContainer from "@/app/_components/CardContainer";
+
+async function getAllAgents() {
+    const data = await fetch("http://localhost:3000/api/accounts/all?role=agent")
+
+    if (!data.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return data.json()
+}
+
+export default async function Agents() {
+    const res = await getAllAgents()
+    console.log(res)
+
     return (
-        <section>
-            <h2>Our Agents</h2>
+        <section className="p-4">
+            <h2 className="text-3xl font-bold">Our Agents</h2>
+            <section className="flex flex-wrap gap-4 my-4">
+                {res && res.results.map(a => (
+                    <CardContainer key={a._id}>
+                        <h3 className="text-xl my-2">{a.username}</h3>
+                        <p className="my-2">{a.email}</p>
+                        <p className="my-2">Managing 0 rental properties</p>
+                        <p className="my-2">Sold: 0 properties</p>
+                    </CardContainer>
+                ))}
+            </section>
         </section>
     )
 }
