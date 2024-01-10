410f9b08848ddcad1556f8e578438a31b068bf04
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-10 18:43:22 +0300

    cleaning up code and minifying it

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -6,19 +6,8 @@ export async function POST(req, res) {
     await connectToDB()
     try {
         const user = await req.json()
-        console.log(user)
 
         await User.create(user)
-            .then(data => {
-                console.log(data)
-            })
-            .catch(err => {
-                console.error(err)
-                return NextResponse.json(
-                    { message: err.message },
-                    { status: 500 }
-                )
-            })
 
         return NextResponse.json(
             { message: "User Created!" },
