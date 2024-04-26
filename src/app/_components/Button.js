840cfa5ffdfa9e9f4402df7346595249bd02a5c8
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-04-26 17:52:17 +0300

    refact: adding on click function

--- src/app/_components/Button.js
+++ src/app/_components/Button.js
@@ -1,5 +1,5 @@
-export default function Button({ children, type }) {
+export default function Button({ children, type, func }) {
     return (
-        <button className="bg-theme-color hover:bg-theme-color/85 py-2 px-4 flex justify-center items-center mx-auto my-6 transition-all hover:rounded-2xl" type={type} >{children}</button>
+        <button className="bg-theme-color hover:bg-theme-color/85 py-2 px-4 flex justify-center items-center mx-auto my-6 transition-all hover:rounded-2xl" type={type} onClick={func} >{children}</button>
     )
 }
