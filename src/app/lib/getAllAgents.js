a5068d543adc9aa1ab84de87a378d1f8e5fc26f0
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-05-06 18:07:00 +0300

    refact: correctly fetching agents

--- src/app/lib/getAllAgents.js
+++ src/app/lib/getAllAgents.js
@@ -1,9 +1,10 @@
 export default async function getAllAgents() {
-    const data = await fetch(`${process.env.BASE_URL}/api/accounts?role=agent`)
+    const data = await fetch(`http://localhost:3000/api/accounts?role=agent`)
+    let result = await data.json()
 
     if (!data.ok) {
         throw new Error('Failed to fetch data')
     }
 
-    return data.json()
+    return result
 }
