9c0a634c3d7419c97a9ba9f3b2fe9009e2c21980
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-08 22:05:04 +0300

    refact: setting property

--- src/app/_components/LocationSearch.js
+++ src/app/_components/LocationSearch.js
@@ -23,7 +23,7 @@ export default function LocationSearch({ setPos, property, setProperty }) {
                 <ul className="flex flex-col divide-y-2 divide-amber-400">
                     {
                         results.map(res => (
-                            <li className="px-4 py-2 w-[100%] flex justify-between items-center cursor-pointer" onClick={() => setPos([res.lat, res.lon])} key={res.osm_id}>
+                            <li className="px-4 py-2 w-[100%] flex justify-between items-center cursor-pointer" onClick={() => {setPos([res.lat, res.lon]); setProperty({...property, location: res.display_name})}} key={res.osm_id}>
                                 <FontAwesomeIcon icon={faMapPin} style={{color: "#E8AE00"}} />{res.display_name}
                             </li>
                         ))
