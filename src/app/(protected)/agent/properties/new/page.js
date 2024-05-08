8c69ef6fc6afb49fae5a180d1dab8f45abe4c098
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-08 20:52:33 +0300

    refact: disabling next button

--- src/app/(protected)/agent/properties/new/page.js
+++ src/app/(protected)/agent/properties/new/page.js
@@ -22,14 +22,14 @@ const AddProperty = () => {
                         <option value="House">House / Apartment</option>
                     </select>
                 </div>
-                <Button func={() => setTab(1)}>Next</Button>
+                <Button func={() => setTab(1)} disabled={type === ""} >Next</Button>
             </section>}
             {tab === 1 && <section className="border-2 rounded-3xl px-8 pt-6 pb-4 w-[90%] text-center">
                 <h2 className="text-2xl my-4 font-semibold">Add a New Property: {type}</h2>
                 <Location property={type === "House" ? property : land} setProperty={type === "House" ? setProperty : setLand} />
                 <div className="flex justify-between w-[100%]">
                     <Button func={() => setTab(0)}>Back</Button>
-                    <Button func={() => setTab(2)}>Next</Button>
+                    <Button func={() => setTab(2)} disabled={type === "House" ? !property.location : !land.location} >Next</Button>
                 </div>
             </section>}
             {tab === 2 && type === "House" && <AddHouseForm property={property} setProperty={setProperty} />}
