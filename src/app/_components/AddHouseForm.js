068ddbdf8a0112a0e622ca5be3e718679f31d9ff
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-05-08 15:59:53 +0300

    feat: add house form

---
+++ src/app/_components/AddHouseForm.js
@@ -0,0 +1,48 @@
+export default function AddHouseForm({ property, setProperty }) {
+    return (
+        <section className="w-[75%]">
+            <h2 className="text-2xl my-4 font-semibold">Add a New Property: House</h2>
+            <form className="w-[100%] flex flex-col gap-4">
+                <div>
+                    <label htmlFor="title">Title: </label>
+                    <input type="text" id="title" value={property.title} onChange={(e) => setProperty({ ...property, title: e.target.value })} className="border-2 border-yellow-300 px-4 py-2 w-[100%]" />
+                </div>
+                <div>
+                    <label htmlFor="desc">Description: </label>
+                    <textarea type="text" id="desc" value={property.desc} onChange={(e) => setProperty({ ...property, desc: e.target.value })} className="border-2 border-yellow-300 px-4 py-2 w-[100%]"></textarea>
+                </div>
+                <div className="flex justify-between items-center">
+                    <div>
+                        <label htmlFor="type">Type: </label>
+                        <select id="type" value={property.type} onChange={(e) => setProperty({ ...property, type: e.target.value })} className="border-2 border-yellow-300 px-4 py-2 w-28">
+                            <option value="Rent">Rental</option>
+                            <option value="Sale">Sale</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="price">Price: </label>
+                        <input type="number" id="price" value={property.price} onChange={(e) => setProperty({ ...property, price: e.target.value })} className="border-2 border-yellow-300 px-4 py-2" />
+                    </div>
+                </div>
+                <div className="flex justify-between items-center">
+                    <div>
+                        <label htmlFor="beds">Bedrooms: </label>
+                        <input type="number" id="beds" value={property.beds} onChange={(e) => setProperty({ ...property, beds: e.target.value })} className="border-2 border-yellow-300 px-4 py-2 w-24 block" />
+                    </div>
+                    <div>
+                        <label htmlFor="baths">Bathrooms: </label>
+                        <input type="number" id="baths" value={property.baths} onChange={(e) => setProperty({ ...property, baths: e.target.value })} className="border-2 border-yellow-300 px-4 py-2 w-24 block" />
+                    </div>
+                    <div>
+                        <label htmlFor="area">Area: </label>
+                        <input type="number" id="area" value={property.area} onChange={(e) => setProperty({ ...property, area: e.target.value })} className="border-2 border-yellow-300 px-4 py-2 w-24 block" />
+                    </div>
+                </div>
+                <div>
+                    <label htmlFor="pictures">Pictures: </label>
+                    <input type="file" multiple id="pictures" value={property.pictures} onChange={(e) => setProperty({ ...property, pictures: e.target.value })} className="px-4 py-2" />
+                </div>
+            </form>
+        </section>
+    )
+}
