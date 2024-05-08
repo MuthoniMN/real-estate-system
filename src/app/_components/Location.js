6a4b34a149ac08175bc553ab221a93aa73f69ad5
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-08 19:40:12 +0300

    refact: removing useMemo

--- src/app/_components/Location.js
+++ src/app/_components/Location.js
@@ -6,13 +6,13 @@ import LocationSearch from "./LocationSearch";
 export default function Location({ property, setProperty }) {
     const [position, setPosition] = useState([-1.286389, 36.817223]);
 
-    const Map = useMemo(() => dynamic(
+    const Map = dynamic(
         () => import('./Map'),
         {
             loading: () => <p>A map is loading</p>,
             ssr: false
         }
-    ), [position])
+    )
 
     return (
         <div className="flex justify-between w-[100%] h-[60vh] items-start">
