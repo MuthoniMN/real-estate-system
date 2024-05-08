16262406cdcfcd389a44eddb9b61ea4d5ba71895
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-05-08 15:57:16 +0300

    feat: adding overflow

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Card from "./_components/Card"
 import Navbar from "./_components/Navbar"
 export default function Home() {
   return (
-    <main>
+    <main className="scroll">
       <Navbar style={{ height: "100px" }} />
       <section style={{ background: "url('https://images.unsplash.com/photo-1496151428281-1d6de673b58e?w=1500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NDl8fHxlbnwwfHx8fHw%3D')", height: "75vh", border: "inset 0 0 0 2000px rgba(0, 0, 0, 0.3)" }} className='bg-fixed bg-no-repeat bg-cover bg-top flex flex-col items-center justify-center'>
         <h1 className='text-3xl text-theme-color font-bold'>Sell. Buy. Rent</h1>
