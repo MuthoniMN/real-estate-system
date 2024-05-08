0c3479636adebf75f6a7a8b25cd29d2241d4fa6f
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-05-08 15:59:21 +0300

    refact: adding scroll

--- src/app/(unauthenticated)/layout.js
+++ src/app/(unauthenticated)/layout.js
@@ -2,9 +2,9 @@ import Navbar from "@/app/_components/Navbar"
 
 export default function UnauthenticatedLayout({ children }) {
     return (
-        <>
+        <main className="scroll">
             <Navbar />
             {children}
-        </>
+        </main>
     )
 }
