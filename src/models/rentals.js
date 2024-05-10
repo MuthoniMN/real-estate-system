8af273b9c714636b33b60fb41bb372b376329b98
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-10 16:30:02 +0300

    refact: adding rentals

---
+++ src/models/rentals.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const RentalsSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: [true, "Please include a price for your property"]
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    beds: {
+        type: Number,
+        required: true
+    },
+    baths: {
+        type: Number,
+        required: true
+    },
+    area: {
+        type: Number
+    },
+    agent: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    pictures: [{
+        imageUrl: {
+            type: String,
+            required: true
+        }
+    }],
+    dueDay: {
+        type: Number,
+        default: 3
+    }
+}, { timestamps: true })
+
+const Rentals = models.Rentals || model('Rentals', RentalsSchema)
+export default Rentals
