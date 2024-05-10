5bdb24259318a0912320e4e8b32eacecbdc20467
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-11 01:45:48 +0300

    feat: admin's view of verified properties

--- src/app/(protected)/admin/property/verified/page.js
+++ src/app/(protected)/admin/property/verified/page.js
@@ -1,7 +1,13 @@
+import PropertiesSection from "@/app/_components/PropertiesSection";
+import { getApprovedProperties, getApprovedLands } from "@/app/lib/properties";
+
 export default function VerifiedProperties() {
+    const houses = await getApprovedProperties();
+    const lands = await getApprovedLands();
     return (
-        <section>
-            <h2>Verified Properties</h2>
-        </section>
+        <main className="flex felx-col gap-8">
+            <PropertiesSection title="Houses and Apartments" max={3} />
+            <PropertiesSection title="Land" max={3} />
+        </main>
     )
 }
