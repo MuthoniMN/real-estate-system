28d7cd6f5310cba2ac8bab11671a43022f1abfd9
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-11 01:47:00 +0300

    refact: admin's view of unverified properties

--- src/app/(protected)/admin/property/unverified/page.js
+++ src/app/(protected)/admin/property/unverified/page.js
@@ -1,7 +1,13 @@
+import PropertiesSection from "@/app/_components/PropertiesSection";
+import { getUnapprovedProperties, getUnapprovedLands } from "@/app/lib/properties";
+
 export default function UnverifiedProperties() {
+    const houses = await getUnapprovedProperties();
+    const lands = await getUnapprovedLands();
     return (
-        <section>
-            <h2>Unverified Properties</h2>
-        </section>
+        <main className="flex felx-col gap-8">
+            <PropertiesSection title="Houses and Apartments" max={3} />
+            <PropertiesSection title="Land" max={3} />
+        </main>
     )
 }
