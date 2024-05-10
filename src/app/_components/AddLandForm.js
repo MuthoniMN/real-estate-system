12a74b8d10620a6aad29a1afc7069b6d3b9cbc5d
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-10 21:28:58 +0300

    refact: optimizing for server action

--- src/app/_components/AddLandForm.js
+++ src/app/_components/AddLandForm.js
@@ -1,81 +1,80 @@
 "use client"
-import { useState } from "react";
-import { notEmpty } from "../utils/validate";
+import { useState, useEffect } from "react";
 import Button from "./Button";
 import ImagePreview from "./ImagePreview"
-import createListing from "../createLandListing";
+import { getUserDetails } from "../lib/getUser";
+import { useSession } from "next-auth/react";
 
-export default function AddLandForm({ land, setLand }) {
+export default function AddLandForm({ location, land, action }) {
     const [success, setSuccess] = useState("")
     const [error, setError] = useState("")
+    const [pictures, setPictures] = useState([]);
+    const [agent, setAgent] = useState("");
+    const { data: session } = useSession()
+
+    useEffect(() => {
+        const getAgent = async () => {
+            let agentId = await getUserDetails(session.user.email)
+            
+            setAgent(agentId)
+        }
+        getAgent()
+        
+    }, [])
+
+
     const upload = (e) => {
         if (e.target.files) {
             const _files = Array.from(e.target.files);
-            setLand({...land, pictures: _files})
-            console.log(land);
+            setPictures( _files)
           }
     }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        console.log(land)
-
-        if(!notEmpty([land.title, land.desc, land.location, land.agent, land.price, land.type, land.dimensions, land.area, land.pictures])){
-            setError("Please fill all the fields!")
-        }
-
-        let createResults = await createListing({...land})
-
-        if(createResults.status == "error"){
-            setError(createResults.message)
-        }else{
-            setSuccess(createResults.message)
-        }
-    }
     return (
         <section className="w-[75%] scroll">
             <h2 className="text-2xl my-4 font-semibold">Add a New Property: Land</h2>
-            <form className="w-[100%] flex flex-col gap-4" onSubmit={(e) => handleSubmit(e)} >
+            <form action={action} className="w-[100%] flex flex-col gap-4" >
                 {success && <p className="bg-green-100 text-green-500">{success}</p>}
                 {error && <p className="bg-red-300 text-red-600">{error}</p>}
+                <input type="hidden" name="agent" value={agent} />
+                <input type="hidden" name="location" value={location || land.location} />
                 <div>
                     <label htmlFor="title">Title: </label>
-                    <input type="text" id="title" value={land.title} onChange={(e) => setLand({ ...land, title: e.target.value })} className="border-2 border-yellow-300 px-4 py-2 w-[100%]" />
+                    <input type="text" id="title" name="title" value={land.title || ""} className="border-2 border-yellow-300 px-4 py-2 w-[100%]" />
                 </div>
                 <div>
                     <label htmlFor="desc">Description: </label>
-                    <textarea id="desc" value={land.desc} aria-describedby="description_help" onChange={(e) => setLand({ ...land, desc: e.target.value })} className="border-2 border-yellow-300 px-4 py-2 w-[100%]"></textarea>
-                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="description_help">{250 - (land.desc ? land.desc.length : 0)} characters remaining</p>
+                    <textarea id="desc" name="desc" value={land.desc || ""} className="border-2 border-yellow-300 px-4 py-2 w-[100%]"></textarea>
                 </div>
                 <div className="flex justify-between items-center">
                     <div>
                         <label htmlFor="type">Type: </label>
-                        <select id="type" value={land.type} onChange={(e) => setLand({ ...land, type: e.target.value })} className="border-2 border-yellow-300 px-4 py-2 w-28">
+                        <select id="type" name="type" value={land.type || ""} className="border-2 border-yellow-300 px-4 py-2 w-28">
                             <option value="Lease">Lease</option>
                             <option value="Sale">Sale</option>
                         </select>
                     </div>
                     <div>
                         <label htmlFor="price">Price: </label>
-                        <input type="number" id="price" value={land.price} onChange={(e) => setLand({ ...land, price: e.target.value })} className="border-2 border-yellow-300 px-4 py-2" />
+                        <input type="number" id="price" name="price" value={land.price || 0} className="border-2 border-yellow-300 px-4 py-2" />
                     </div>
                 </div>
                 <div className="flex justify-between items-center w-[100%]">
                     <div>
                         <label htmlFor="dimensions">Dimensions: </label>
-                        <input type="text" id="dimensions" value={land.dimensions} onChange={(e) => setLand({ ...land, dimensions: e.target.value })} placeholder="50 * 100" className="border-2 border-yellow-300 px-4 py-2 block" />
+                        <input type="text" id="dimensions" name="dimensions" value={land.dimensions || 0} placeholder="50 * 100" className="border-2 border-yellow-300 px-4 py-2 block" />
                     </div>
                     <div>
                         <label htmlFor="area">Area: </label>
-                        <input type="number" id="area" value={land.area} onChange={(e) => setLand({ ...land, area: e.target.value })} className="border-2 border-yellow-300 px-4 py-2 w-24 block" />
+                        <input type="number" id="area" name="area" value={land.area || 0} className="border-2 border-yellow-300 px-4 py-2 w-24 block" />
                     </div>
                 </div>
                 <div>
                     <label htmlFor="pictures">Pictures: </label>
-                    <input type="file" multiple id="pictures" accept="image/*" aria-describedby="file_input_help" onChange={(e) => upload(e)} className="px-4 py-2 file:bg-theme-color file:px-4 file:py-2 file:transition-all hover:file:rounded-2xl hover:file:bg-theme-color/85" />
+                    <input type="file" multiple id="pictures" name="pictures" accept="image/*" aria-describedby="file_input_help" onChange={(e) => upload(e)} className="px-4 py-2 file:bg-theme-color file:px-4 file:py-2 file:transition-all hover:file:rounded-2xl hover:file:bg-theme-color/85" />
                     <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
                 </div>
-                { land.pictures && <ImagePreview images={land.pictures} />}
+                { (pictures || land.pictures ) && <ImagePreview images={pictures || land.pictures} />}
                 <Button type="submit">Add Property</Button>
             </form>
         </section>
