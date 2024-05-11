968a844d2a09dc49b678f74f690423b5b88717a3
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-11 15:07:13 +0300

    bug: show pagination when there is properties

--- src/app/_components/PropertiesSection.js
+++ src/app/_components/PropertiesSection.js
@@ -4,6 +4,7 @@ import Pagination from "./Pagination"
 import Property from "./Property"
 
 export default function PropertiesSection( { title, properties, max } ){
+    console.log(properties);
     const [currentPage, setCurrentPage] = useState(1);
     const end = currentPage * max
     const start = end - max
@@ -14,9 +15,9 @@ export default function PropertiesSection( { title, properties, max } ){
             <h2>{title}</h2>
             <div className='flex justify-between items-center w-full h-full'>
                 { page && page.map(property => <Property property={property} small key={property._id} />)}
-                {!properties && (<p>No Properties Yet...</p>) }
+                {properties === [] && (<p>No Properties Yet...</p>) }
             </div>
-            <Pagination total={properties.title} max={max} current={currentPage} setCurrent={setCurrentPage} />
+            {properties && <Pagination total={properties.title} max={max} current={currentPage} setCurrent={setCurrentPage} />}
         </section>
     )
 }
