9a7f13d34edf3be4ddcea5509c0430668c2e3a35
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-11 15:39:49 +0300

    refact: changing height

--- src/app/_components/PropertiesSection.js
+++ src/app/_components/PropertiesSection.js
@@ -12,9 +12,9 @@ export default function PropertiesSection( { title, properties, max } ){
 
     if(properties.length > 0 ) page = properties.splice(start, end)
     return (
-        <section className='w-full h-full'>
+        <section className='w-full h-[100%] py-8'>
             <h2 className='text-3xl font-bold' >{title}</h2>
-            <div className='flex justify-between items-center w-full h-full'>
+            <div className='flex justify-between items-center w-full h-[100%]'>
                 { page && page.map(property => <Property property={property} small key={property._id} />)}
                 {properties.length === 0 && (<p>No Properties Yet...</p>) }
             </div>
