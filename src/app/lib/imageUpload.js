9b71981b08deb164cd7c19486770aad602827a0b
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-14 23:54:40 +0300

    refact: removing signature

--- src/app/lib/imageUpload.js
+++ src/app/lib/imageUpload.js
@@ -1,37 +1,19 @@
-const crypto = require('crypto')
-
-const generateSignature = (public_id, timestamp, transform) => {
-    const string = `eager=${transform}&public_id=${public_id}&timestamp=${timestamp}${process.env.CLOUDINARY_API_SECRET}`
-
-    const signature = crypto
-                                    .createHash('sha1')
-                                    .update(string)
-                                    .digest('hex')
-
-    return signature
-}
-
 export const uploadImages = async (fileList, property) => {
     let index = 0;
     let images = []
 
     while (index < fileList.length){
         const formdata = new FormData();
-        const timestamp = Date.now();
         const public_id = `${property.replace(' ', '_')}-${index}`
-        const sign = generateSignature(public_id, timestamp, "w_400,h_400,c_fit")
 
         formdata.append("file", URL.createObjectURL(fileList.item(index))) 
         formdata.append("public_id", public_id)
-        formdata.append("signature", sign)
         formdata.append("eager", "w_400,h_400,c_fit" )
-        formdata.append("api_key", process.env.CLOUDINARY_API_KEY)
-        formdata.append("timestamp", timestamp)
+        formData.append("upload_preset", "property_pictures");
 
         const requestOptions = {
             method: "POST",
-            body: formdata,
-            redirect: "follow"
+            body: formdata
           };
           
           try{
