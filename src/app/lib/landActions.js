1aacbd5ac6862eff4fb3c7bbadd62da488fa0620
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-14 21:51:43 +0300

    refact: validating the fields and uploading the pictures to cloudinary

--- src/app/lib/landActions.js
+++ src/app/lib/landActions.js
@@ -1,11 +1,35 @@
 "use server"
 import Land from "@/models/land";
 import { connectToDB } from "../utils/db";
+import { landSchema } from '@/app/_schemas/property'
+import { uploadImages } from "./imageUpload";
 
-export default async function createListing(land){
+export default async function createListing(agent, location, land){
+    const landObj = Object.fromEntries(land)
+
+    //validate fields
+    const validatedFields = landSchema.safeParse(landObj)
+     
+      if (!validatedFields.success) {
+        return {
+          errors: validatedFields.error.issues,
+        }
+      }
+    
+      // upload images
+      const images = await uploadImages(land.pictures)
+      if(images.status === "error"){
+        return {
+          errors: {
+            path: ["pictures"],
+            message: images.error
+          }
+        }
+      }
+
     try {
         await connectToDB();
-        await Land.create(land)
+        await Land.create({ ...landObj, agent: agent, location: location, pictures: images  })
         return {
             status: "success", 
             message: "Listing was successfully created!"
