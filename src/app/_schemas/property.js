b5b392d8fea2a3bc7f6ebca7138d16befec4e753
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-14 21:49:08 +0300

    feat: defining land schema

---
+++ src/app/_schemas/property.js
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const landSchema = z.object({
+    title: z.string({
+      required_error: 'Please enter a title',
+    }),
+    desc: z.string({
+        required_error: 'Please enter the property description',
+      }),
+    type: z.string({
+        required_error: 'Please enter the property type',
+    }),
+    price: z.number({
+        required_error: "Price is required",
+        invalid_type_error: "Price must be a number",
+    }),
+    dimensions: z.string({
+        required_error: 'Please enter the dimensions',
+    }),
+    area: z.number({
+        required_error: "Please enter the area"
+    }),
+    pictures: z
+    .instanceof(File, { message: 'Please upload a file.'})
+    .refine((f) => f.size < 100_000, 'Max 100 kB upload size.')
+    .array()
+  })
