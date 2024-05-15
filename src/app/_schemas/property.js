51e5162a5b6716e1ab1e13ee8e948af430eedd20
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-15 10:00:47 +0300

    feat: adding property schema

--- src/app/_schemas/property.js
+++ src/app/_schemas/property.js
@@ -25,3 +25,32 @@ export const landSchema = z.object({
     .refine((f) => f.size < 100_000, 'Max 100 kB upload size.')
     .array()
   })
+
+  export const propertySchema = z.object({
+    title: z.string({
+      required_error: 'Please enter a title',
+    }),
+    desc: z.string({
+        required_error: 'Please enter the property description',
+      }),
+    type: z.string({
+        required_error: 'Please enter the property type',
+    }),
+    price: z.number({
+        required_error: "Price is required",
+        invalid_type_error: "Price must be a number",
+    }),
+    area: z.number({
+        required_error: "Please enter the area"
+    }),
+    beds: z.number({
+      required_error: "Please enter the number of bedrooms"
+  }),
+  baths: z.number({
+    required_error: "Please enter the number of bathrooms"
+}),
+    pictures: z
+    .instanceof(File, { message: 'Please upload a file.'})
+    .refine((f) => f.size < 100_000, 'Max 100 kB upload size.')
+    .array()
+  })
