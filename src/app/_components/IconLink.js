c046c1c375d55f4ce0a91221b5ca994de05cc46b
Michelle Ndiangui <105012834+MuthoniMN@users.noreply.github.com>
2024-05-15 09:46:58 +0300

    refact: making width 100% in medium screens

--- src/app/_components/IconLink.js
+++ src/app/_components/IconLink.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export default function IconLink({ link, icon, linkDesc }) {
     return (
-        <Link href={link} className="flex gap-2 items-center justify-between">
+        <Link href={link} className="flex gap-2 items-center justify-between md:w-full">
             <FontAwesomeIcon icon={icon} style={{ height: "32px" }} />
             <span className="hidden md:block w-full">{linkDesc}</span>
         </Link>
